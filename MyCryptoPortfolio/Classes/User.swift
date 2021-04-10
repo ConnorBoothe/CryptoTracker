70134f60f22077bd2e0cf4f236097233f1c8bf85
Connor Boothe <connorboothe@gmail.com>
2021-04-10 17:29:28 -0400

    Added User class

---
+++ MyCryptoPortfolio/Classes/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  MyCryptoPortfolio
+//
+//  Created by Connor Boothe on 4/7/21.
+//
+
+import Foundation
+
+struct User: Decodable {
+    var first_name: String
+    var last_name: String
+    var email: String
+    var password: String
+}
+
+
