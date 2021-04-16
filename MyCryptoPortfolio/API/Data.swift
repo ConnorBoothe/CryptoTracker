e5fd199fdb1136e5b1e9df93606bd3663a1078a9
Connor Boothe <connorboothe@gmail.com>
2021-04-15 22:17:54 -0400

    Added SellCoin()

--- MyCryptoPortfolio/API/Data.swift
+++ MyCryptoPortfolio/API/Data.swift
@@ -15,7 +15,6 @@ class API {
         var coins:Array<Coin> = [];
         //loop through assets and make API call for each asset
         for (coin) in assets {
-            print(coin)
             //define the headers for the API request
         let headers = [
             "x-rapidapi-key": "66d9a43a5amsh346768cb83dc90bp1bee3ejsn264e836c8915",
@@ -45,11 +44,14 @@ class API {
 
                         let newCoin = Coin(name: coin_name as! String, image: URL(string:thumbnail)!, price: usd_price, ticker: ticker as!String, amount:coin.amount, desc: simple_desc);
                             coins.append(newCoin)
-                   
+                        if(coins.count  == assets.count){
                             priceCompletionHandler(coins);
+                        }
+                           
                         }
                 }
                 catch {
+                    print(error)
                     print("Err")
                 }
             }
@@ -58,7 +60,7 @@ class API {
         
      }
     }
-    func getMarketData(assets:Array<Coins>, priceCompletionHandler: @escaping (Array<Coin>) -> Void){
+    func getMarketData(assets:Array<String>, priceCompletionHandler: @escaping (Array<Coin>) -> Void){
         var coins:Array<Coin> = [];
         //loop through assets and make API call for each asset
         for (coin) in assets {
@@ -68,7 +70,7 @@ class API {
             "x-rapidapi-host": "coingecko.p.rapidapi.com"
         ]
 
-            let request = NSMutableURLRequest(url: NSURL(string: "https://coingecko.p.rapidapi.com/coins/\(coin.name)?developer_data=true&market_data=true&sparkline=false&community_data=true&localization=true&tickers=true")! as URL,
+            let request = NSMutableURLRequest(url: NSURL(string: "https://coingecko.p.rapidapi.com/coins/\(coin)?developer_data=true&market_data=true&sparkline=false&community_data=true&localization=true&tickers=true")! as URL,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
         request.httpMethod = "GET"
@@ -90,7 +92,7 @@ class API {
                             let market_data = convertedJsonIntoDict["market_data"] as? NSDictionary
                             let currPrice = market_data!["current_price"] as? NSDictionary
                                 usd_price = currPrice!["usd"] as! Double
-                        var newCoin = Coin(name: coin_name as! String, image: URL(string:thumbnail)!, price: usd_price, ticker: ticker as!String, amount:coin.amount, desc: simple_desc);
+                        var newCoin = Coin(name: coin_name as! String, image: URL(string:thumbnail)!, price: usd_price, ticker: ticker as!String, amount:0.00, desc: simple_desc);
                             coins.append(newCoin)
                             //needs to complete with an array of coin objects
                         if(coins.count == assets.count) {
@@ -123,7 +125,6 @@ class API {
         return String(priceArray);
     }
     func getUser(email:String, password:String,completionHandler: @escaping (User) -> Void){
-        print("get user")
         let request = NSMutableURLRequest(url: NSURL(string: "http://127.0.0.1:8080/API/GetUser/\(email)/\(password)")! as URL,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
@@ -137,7 +138,6 @@ class API {
                     if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data!, options: []) as? Array<Any> {
                         var user = convertedJsonIntoDict[0] as? NSDictionary;
 //                        var first_name = user.first_name;
-                        print(user)
                         let first_name = user!["first_name"]! as? String
                         let last_name = user!["last_name"]! as? String
                         let email = user!["email"]! as? String
@@ -174,17 +174,12 @@ class API {
             "email":email,
             "password": password
         ];
-        let valid = JSONSerialization.isValidJSONObject(params)
-        print(valid)
         let request = NSMutableURLRequest(url: NSURL(string: "http://127.0.0.1:8080/API/AddUser/")! as URL,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
         request.httpMethod = "POST"
         let jsonData = try? JSONSerialization.data(withJSONObject: params)
-        print(jsonData)
         request.httpBody = try? JSONSerialization.data(withJSONObject: params);
-        print(request.httpBody)
-        print(request)
         //HTTP Headers
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
@@ -195,6 +190,7 @@ class API {
                 print(error!)
             } else {
                 do {
+                    
                     print("added")
                     completionHandler("Done")
                 }
@@ -207,14 +203,14 @@ class API {
         dataTask.resume()
         
     }
-    func addCoin(name:String, amount:String, email:String, completionHandler: @escaping (String) -> Void){
+    func addCoin(name:String, amount:String, email:String, completionHandler: @escaping (Array<Coins>) -> Void){
         let params:[String: String]  = [
             "name": name,
             "amount": amount,
             "email":email
         ];
         let valid = JSONSerialization.isValidJSONObject(params)
-        print(valid)
+
         let request = NSMutableURLRequest(url: NSURL(string: "http://127.0.0.1:8080/API/AddCoin")! as URL,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
@@ -229,8 +225,60 @@ class API {
                 print(error!)
             } else {
                 do {
-                    print("added")
-                    completionHandler("Done")
+
+                    if let user = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
+                    let coins = user["coins"]! as! Array<NSDictionary>
+               
+                    var new_coin_array:Array<Coins> = [];
+                    for (coin) in coins {
+                        new_coin_array.append(Coins(name:coin["name"]! as! String, amount: coin["amount"]! as! Double))
+                    }
+                    completionHandler(new_coin_array)
+                    }
+
+                }
+                catch {
+                    print("error here")
+                    print(error)
+                }
+            }
+        })
+        dataTask.resume()
+        
+    }
+    func sellCoin(name:String, amount:String, email:String, completionHandler: @escaping (Array<Coins>) -> Void){
+        let params:[String: String]  = [
+            "name": name,
+            "amount": amount,
+            "email":email
+        ];
+        let valid = JSONSerialization.isValidJSONObject(params)
+        print(valid)
+        let request = NSMutableURLRequest(url: NSURL(string: "http://127.0.0.1:8080/API/SellCoin")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: params);
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print("error here")
+                print(error!)
+            } else {
+                do {
+
+                    if let user = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
+                    let coins = user["coins"]! as! Array<NSDictionary>
+               
+                    var new_coin_array:Array<Coins> = [];
+                    for (coin) in coins {
+                        new_coin_array.append(Coins(name:coin["name"]! as! String, amount: coin["amount"]! as! Double))
+                    }
+                    completionHandler(new_coin_array)
+                    }
+
                 }
                 catch {
                     print("error here")
